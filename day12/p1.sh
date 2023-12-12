cba8b3efb9561d34420a99f19e29587b6797e323
Sarah Schulte <git@sarah.engineer>
2023-12-12 01:30:56 -0800

    part 1

--- day12/p1.sh
+++ day12/p1.sh
@@ -1,58 +1,92 @@
 #!/bin/bash
 
-clear
+set -o noglob
 
-FILE=sample.txt
+
+FILE=input.txt
+
+function printl() {
+  return
+  local indent=$1
+  printf "%*s" $indent "" 1>&2
+  shift
+  printf "%s\n" "$*" 1>&2
+}
 
 function descend() {
-  local record=$1
-  local sizes=$2
-  local iter=$3
-  local size=${sizes%%,*}
-  local count=0
-  local hard=0
+  local record="$1"
+  local sizes="$2"
+  local iter="$3"
+  local sliced="$4"
+
+  if [[ $iter -eq 0 ]]; then
+    local left=${record//[^#]/}
+    if [[ ${#left} -eq 0 ]]; then
+      printl 0 "------------"
+      echo "------------"
+    fi
+    return
+  fi
+
+  # pop size from sizes
+  local size="${sizes%%,*}"
+  sizes="${sizes#*,}"
+
+  if [[ -z "$record" ]]; then
+    return
+  fi
+  printl $sliced $record $size "depth:$iter"
+
+
   local idx=0
+  local char
   while read -n1 char; do
     if [[ "$char" == "" ]]; then
       continue
     fi
-    if [[ "$char" == "#" ]] || [[ "$char" == "?" ]]; then
-      if [[ "$char" == "#" ]]; then
-        ((hard++))
-      fi
-      ((count++))
-      if [[ $count -ge $size ]]; then
-        if [[ ${record:((idx+1)):1} != "#" ]]; then
-          if [[ "$sizes" =~ , ]]; then
-            #
-            descendant=${record:((idx+2))}
-            if [[ ! -z $descendant ]]; then
-              echo "$iter       $record"
-              echo descend ${record:((idx+2))} ${sizes#*,}
-              echo
-              descend $descendant ${sizes#*,} $((iter+1))
-            fi
-          fi
-        fi
-      fi
-    elif [[ "$char" == "." ]]; then
-      if [[ $count -ge $size ]]; then
-        ((idx++))
-        break
-      fi
-      count=0
+    local slice="${record:idx:size}"
+    if [[ ${#slice} -lt $size ]]; then
+      return
     fi
+    if [[ ${slice:0:1} == "." ]]; then
+      ((idx++))
+      continue
+    fi
+    printl 0 "$iter --> $slice"
+    group=${slice//[^?#]/}
+    if [[ ${#group} -lt $size ]]; then
+      if [[ "${slice:0:1}" == "#" ]]; then
+        return
+      fi
+      ((idx++))
+      continue
+    fi
+    local peek="${record:((idx+size)):1}"
+    if [[ "$peek" != "#" ]]; then
+      local descendant="${record:((idx+size+1))}"
+      descend "$descendant" "${sizes:-0}" "$((iter-1))" "$((sliced+idx+size+1))"
+    fi
+    # check if we hit obsidian
+    if [[ "${slice:0:1}" == "#" ]]; then
+      return
+    fi
+    # wtf was the point of this? idk
+    # check if we hit air
+    # if [[ "${record:((idx+size)):1}" == "." ]]; then
+    #   return
+    # fi
     ((idx++))
   done <<< "$record"
-  if [[ "$sizes" != *,* ]] && [[ $count -ge $size ]]; then
-    :
-    echo "$record"
-    echo "SOLUTIONS: " $((count-size+1))
-  fi
 }
 
 while read record sizes; do
-  descend $record $sizes 0
+  maxdepth=${sizes//[^,]/}
+  maxdepth=${#maxdepth}
+  ((maxdepth++))
+  # echo "$record $sizes"
+  descend "$record" "$sizes" "$maxdepth" 0 \
+    | wc -l
 
-  break
-done < "$FILE"
+done < "$FILE" \
+  | paste -sd + \
+  | bc
