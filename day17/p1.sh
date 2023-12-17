44daa194d4d66c594e0c3ab4ab7c2e5a4de466cc
Sarah Schulte <git@sarah.engineer>
2023-12-17 09:32:03 -0800

    clean up time

--- day17/p1.sh
+++ day17/p1.sh
@@ -48,7 +48,6 @@ heapify_up() {
         parent="${priority_queue[$parent_index]##*,}"
         self="${priority_queue[$index]##*,}"
         if [ $self -lt $parent ]; then
-            # Swap elements if the current element is smaller than its parent
             local temp=${priority_queue[$index]}
             priority_queue[$index]=${priority_queue[$parent_index]}
             priority_queue[$parent_index]=$temp
@@ -81,7 +80,6 @@ heapify_down() {
       fi
 
       if [ "$smallest_idx" -ne "$index" ]; then
-          # Swap elements if the current element is greater than the smallest_idx child
           local temp=${priority_queue[$index]}
           priority_queue[$index]=${priority_queue[$smallest_idx]}
           priority_queue[$smallest_idx]=$temp
@@ -97,15 +95,13 @@ width=${#grid[0]}
 height=$(wc -l < $FILE | cut -d' ' -f2)
 target=$((height-1)),$((width-1))
 echo "TARGET: $target"
-# row=${grid[1]}
-# echo "${row:1:1}"
 
 function compute_cost() {
   local ur=$1
   local uc=$2
   local vr=$3
   local vc=$4
-  # echo -n "COST FROM $ur,$uc -> $vr,$vc: "
+
   dr=$((vr-ur))
   dc=$((vc-uc))
   if [[ $dr -gt 0 ]]; then
@@ -127,9 +123,9 @@ function compute_cost() {
     ((vr-=dr))
     ((vc-=dc))
   done
-  # echo "$cost"
 }
 
+# do A* pathfinding
 enqueue V,0,0,0
 enqueue H,0,0,0
 declare -A cameFrom
@@ -151,14 +147,10 @@ done
 while [[ ${#priority_queue[@]} -gt 0 ]]; do
   dequeue
   current=${front#*,}
-  # if [[ $current == "$target" ]]; then
-  #   echo "DONE"
-  #   break
-  # fi
 
   u=$front
   IFS=, read ul ur uc <<< "$u"
-  # for each neighbor...
+  # like a good neighbor...
   for v in \
     "H,$((ur-1)),$uc,U" \
     "H,$((ur-2)),$uc,U" \
@@ -189,52 +181,27 @@ while [[ ${#priority_queue[@]} -gt 0 ]]; do
     ((tentative_gScore+=cost))
     if [[ $tentative_gScore -lt ${gScore[$v]} ]]; then
       cameFrom[$v]=$u
-      # echo "$u -> $v"
       gScore[$v]=$tentative_gScore
-      # fScore[$v]=$tentative_gScore
       fScore[$v]=$((tentative_gScore + vr * vc))
       enqueue_if_missing $v ${fScore[$v]}
     fi
   done
 done
-    # lookback1=${prev[$u]}
-    # l1=${lookback1%,*}
-    # d1=${lookback1##*,}
-    # if [[ $d == $d1 ]]; then
-    #   continue
-    # fi
-    # if [[ $d == "U" ]] && [[ $d1 == "D" ]]; then
-    #   continue
-    # fi
-    # if [[ $d == "D" ]] && [[ $d1 == "U" ]]; then
-    #   continue
-    # fi
-    # if [[ $d == "R" ]] && [[ $d1 == "L" ]]; then
-    #   continue
-    # fi
-    # if [[ $d == "L" ]] && [[ $d1 == "R" ]]; then
-    #   continue
-    # fi
+
+# find solutions
 for level in V H; do
   declare -a sequence
   sequence=()
-  # u ← target
   u=$level,$target
-  # if prev[u] is defined or u = source:
   if [[ ! -z ${cameFrom[$u]} ]]; then
-  #     while u is defined:
     while [[ ! -z "$u" ]]; do
-  #         insert u at the beginning of S
       sequence=("$u" "${sequence[@]}")
-  #         u ← cameFrom[u]
       u=${cameFrom[$u]}
-      # echo "'$u'"
     done
   fi
   counter=0
   prev=$level,0,0
   for cell in "${sequence[@]:1}"; do
-    # echo "$cell"
     IFS=, read vl vr vc <<< "$cell"
     IFS=, read ul ur uc <<< "$prev"
     compute_cost $ur $uc $vr $vc
