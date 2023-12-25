96f016ddde0c63170f83bf2e44a80d1fe2a83297
Sarah Schulte <git@sarah.engineer>
2023-12-25 06:14:00 -0800

    this approach was MUCH better

--- day24/p2.sh
+++ day24/p2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 clear
-FILE=sample.txt
+FILE=input.txt
 
-function matrix() {
+function solve() {
   local -a px py pz vx vy vz cx cy cz
   while IFS=, read ipx ipy ipz ivx ivy ivz; do
     px+=("$ipx")
@@ -13,85 +13,95 @@ function matrix() {
     vy+=("$ivy")
     vz+=("$ivz")
   done
+echo "
+scale=10;
+vax=${vx[0]};
+vbx=${vx[1]};
+vcx=${vx[2]};
+vdx=${vx[3]};
+vex=${vx[4]};
+pax=${px[0]};
+pbx=${px[1]};
+pcx=${px[2]};
+pdx=${px[3]};
+pex=${px[4]};
+vay=${vy[0]};
+vby=${vy[1]};
+vcy=${vy[2]};
+vdy=${vy[3]};
+vey=${vy[4]};
+pay=${py[0]};
+pby=${py[1]};
+pcy=${py[2]};
+pdy=${py[3]};
+pey=${py[4]};
+        denominator = (((vay - vby) * (pbx - pcx) - (vby - vcy) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vby - vcy) * (pcx - pdx) - (vcy - vdy) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx))) * (((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcx - vdx) * (pdx - pex) - (vdx - vex) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) - (((vby - vcy) * (pcx - pdx) - (vcy - vdy) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcy - vdy) * (pdx - pex) - (vdy - vey) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) * (((vax - vbx) * (pbx - pcx) - (vbx - vcx) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)));
 
-  cx[0]=$(( py[1]*vz[1]-vy[1]*pz[1] - (py[0]*vz[0]-vy[0]*pz[0]) ))
-  cy[0]=$(( pz[1]*vx[1]-vx[1]*pz[1] - (pz[0]*vz[0]-vx[0]*pz[0]) ))
-  cz[0]=$(( px[1]*vy[1]-vy[1]*px[1] - (px[0]*vy[0]-vy[0]*px[0]) ))
-  cx[1]=$(( py[2]*vz[2]-vy[2]*pz[2] - (py[0]*vz[0]-vy[0]*pz[0]) ))
-  cy[1]=$(( pz[2]*vx[2]-vx[2]*pz[2] - (pz[0]*vz[0]-vx[0]*pz[0]) ))
-  cz[1]=$(( px[2]*vy[2]-vy[2]*px[2] - (px[0]*vy[0]-vy[0]*px[0]) ))
-  echo -n $((vy[1]-vy[0])) $((vx[0]-vx[1])) 0" "
-  echo -n $((vy[2]-vy[0])) $((vx[0]-vx[2])) 0" "
-  echo ${cz[0]}
-  echo -n 0 $((pz[0]-pz[1])) $((py[1]-py[0]))" "
-  echo -n 0 $((pz[0]-pz[2])) $((py[2]-py[0]))" "
-  echo ${cx[1]}
-  echo -n $((vz[0]-vz[1])) 0 $((vx[1]-vx[0]))" "
-  echo -n $((vz[0]-vz[2])) 0 $((vx[2]-vx[0]))" "
-  echo ${cy[0]}
-  echo -n $((pz[1]-pz[0])) 0 $((px[0]-px[1]))" "
-  echo -n $((pz[2]-pz[0])) 0 $((px[0]-px[2]))" "
-  echo ${cy[1]}
-  echo -n $((py[0]-py[1])) $((px[1]-px[0])) 0" "
-  echo -n $((py[0]-py[2])) $((px[2]-px[0])) 0" "
-  echo ${cz[1]}
-  echo -n 0 $((vz[1]-vz[0])) $((vy[0]-vy[1]))" "
-  echo -n 0 $((vz[2]-vz[0])) $((vy[0]-vy[2]))" "
-  echo ${cx[0]}
-  # echo 0 $((vz[1]-vz[0])) $((vy[0]-vy[1]))
-  # echo $((vz[0]-vz[1])) 0 $((vx[1]-vx[0]))
-  # echo $((vy[1]-vy[0])) $((vx[0]-vx[1])) 0
-  # echo
-  # echo 0 $((vz[2]-vz[0])) $((vy[0]-vy[2]))
-  # echo $((vz[0]-vz[2])) 0 $((vx[2]-vx[0]))
-  # echo $((vy[2]-vy[0])) $((vx[0]-vx[2])) 0
-  # echo
-  # echo 0 $((pz[0]-pz[1])) $((py[1]-py[0]))
-  # echo $((pz[1]-pz[0])) 0 $((px[0]-px[1]))
-  # echo $((py[0]-py[1])) $((px[1]-px[0])) 0
-  # echo
-  # echo 0 $((pz[0]-pz[2])) $((py[2]-py[0]))
-  # echo $((pz[2]-pz[0])) 0 $((px[0]-px[2]))
-  # echo $((py[0]-py[2])) $((px[2]-px[0])) 0
-}
+        numerator = (((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)) * (((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pcx - pdx) - ((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pbx - pcx)) - ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) * (((pax*vay - pbx*vby) - (pay*vax - pby*vbx)) * (pbx - pcx) - ((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pax - pbx))) * (((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcx - vdx) * (pdx - pex) - (vdx - vex) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) - (((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) * (((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pdx - pex) - ((pdx*vdy - pex*vey) - (pdy*vdx - pey*vex)) * (pcx - pdx)) - ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) * (((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pcx - pdx) - ((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pbx - pcx))) * (((vax - vbx) * (pbx - pcx) - (vbx - vcx) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)));
 
-function add_row() {
-    local multiple=$1
-    local source=$2
-    local dest=$3
-    local i
-    for ((i=0;i<7;i++)); do
-      grid["$dest,$i"]=$(echo "scale=2; ${grid["$dest,$i"]} + ${grid["$source,$i"]}*$multiple" | bc)
-    done
-}
+      - numerator / denominator
+" | bc
+echo "
+scale=10;
+vax=${vy[0]};
+vbx=${vy[1]};
+vcx=${vy[2]};
+vdx=${vy[3]};
+vex=${vy[4]};
+pax=${py[0]};
+pbx=${py[1]};
+pcx=${py[2]};
+pdx=${py[3]};
+pex=${py[4]};
+vay=${vz[0]};
+vby=${vz[1]};
+vcy=${vz[2]};
+vdy=${vz[3]};
+vey=${vz[4]};
+pay=${pz[0]};
+pby=${pz[1]};
+pcy=${pz[2]};
+pdy=${pz[3]};
+pey=${pz[4]};
+        denominator = (((vay - vby) * (pbx - pcx) - (vby - vcy) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vby - vcy) * (pcx - pdx) - (vcy - vdy) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx))) * (((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcx - vdx) * (pdx - pex) - (vdx - vex) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) - (((vby - vcy) * (pcx - pdx) - (vcy - vdy) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcy - vdy) * (pdx - pex) - (vdy - vey) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) * (((vax - vbx) * (pbx - pcx) - (vbx - vcx) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)));
 
-declare -A grid
-function echelon() {
-  row=0
-  while read a b c d e f g; do
-    grid["$row,0"]=$((a * 1))
-    grid["$row,1"]=$((b * 1))
-    grid["$row,2"]=$((c * 1))
-    grid["$row,3"]=$((d * 1))
-    grid["$row,4"]=$((e * 1))
-    grid["$row,5"]=$((f * 1))
-    grid["$row,6"]=$((g * 1))
-    ((row++))
-  done
-  for ((col=0; col<1; col++)); do
-    for((i=col+1;i<6;i++)); do
-      add_row $(echo "scale=2; ${grid["$i,$((col+1))"]} / ${grid["$col,$col"]}" | bc) $col $i
-    done
-  done
-  for((row=0;row<6;row++)); do
-    for((col=0;col<7;col++)); do
-      echo -n ${grid["$row,$col"]}" "
-    done
-    echo
-  done
+        numerator = (((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)) * (((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pcx - pdx) - ((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pbx - pcx)) - ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) * (((pax*vay - pbx*vby) - (pay*vax - pby*vbx)) * (pbx - pcx) - ((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pax - pbx))) * (((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcx - vdx) * (pdx - pex) - (vdx - vex) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) - (((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) * (((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pdx - pex) - ((pdx*vdy - pex*vey) - (pdy*vdx - pey*vex)) * (pcx - pdx)) - ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) * (((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pcx - pdx) - ((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pbx - pcx))) * (((vax - vbx) * (pbx - pcx) - (vbx - vcx) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)));
+
+      - numerator / denominator
+" | bc
+echo "
+scale=10;
+vax=${vz[0]};
+vbx=${vz[1]};
+vcx=${vz[2]};
+vdx=${vz[3]};
+vex=${vz[4]};
+pax=${pz[0]};
+pbx=${pz[1]};
+pcx=${pz[2]};
+pdx=${pz[3]};
+pex=${pz[4]};
+vay=${vx[0]};
+vby=${vx[1]};
+vcy=${vx[2]};
+vdy=${vx[3]};
+vey=${vx[4]};
+pay=${px[0]};
+pby=${px[1]};
+pcy=${px[2]};
+pdy=${px[3]};
+pey=${px[4]};
+        denominator = (((vay - vby) * (pbx - pcx) - (vby - vcy) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vby - vcy) * (pcx - pdx) - (vcy - vdy) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx))) * (((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcx - vdx) * (pdx - pex) - (vdx - vex) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) - (((vby - vcy) * (pcx - pdx) - (vcy - vdy) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcy - vdy) * (pdx - pex) - (vdy - vey) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) * (((vax - vbx) * (pbx - pcx) - (vbx - vcx) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)));
+
+        numerator = (((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)) * (((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pcx - pdx) - ((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pbx - pcx)) - ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) * (((pax*vay - pbx*vby) - (pay*vax - pby*vbx)) * (pbx - pcx) - ((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pax - pbx))) * (((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) - ((vcx - vdx) * (pdx - pex) - (vdx - vex) * (pcx - pdx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx))) - (((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) * (((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pdx - pex) - ((pdx*vdy - pex*vey) - (pdy*vdx - pey*vex)) * (pcx - pdx)) - ((pcy-pdy) * (pdx - pex) - (pdy-pey) * (pcx - pdx)) * (((pbx*vby - pcx*vcy) - (pby*vbx - pcy*vcx)) * (pcx - pdx) - ((pcx*vcy - pdx*vdy) - (pcy*vcx - pdy*vdx)) * (pbx - pcx))) * (((vax - vbx) * (pbx - pcx) - (vbx - vcx) * (pax - pbx)) * ((pby-pcy) * (pcx - pdx) - (pcy-pdy) * (pbx - pcx)) - ((vbx - vcx) * (pcx - pdx) - (vcx - vdx) * (pbx - pcx)) * ((pay-pby) * (pbx - pcx) - (pby-pcy) * (pax - pbx)));
+
+      - numerator / denominator
+" | bc
+
 }
-head -n3 $FILE \
+head -n5 $FILE \
   | tr '@' ',' \
   | tr -d ' ' \
-  | matrix \
-  | echelon
+  | solve \
+  | paste -sd+ \
+  | bc
