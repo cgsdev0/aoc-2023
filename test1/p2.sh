6c1674e343fe75b5e13dc4912eb71375b681befc
Sarah Schulte <git@sarah.engineer>
2024-11-30 21:38:04 -0800

    part 2 ez but not elegant

--- test1/p2.sh
+++ test1/p2.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 clear
+
+FILE=input.txt
+
+declare -A counts
+while read c n; do
+  counts[$n]=$c
+done < <(tr -s ' ' < "$FILE" \
+  | cut -d' ' -f2 \
+  | sort -n \
+  | uniq -c \
+  | tr -s ' ' \
+  | cut -d' ' -f2-3)
+
+while read n; do
+  echo "$n*${counts[$n]:-0}"
+done < <(tr -s ' ' < "$FILE" | cut -d' ' -f1) \
+  | paste -sd+ \
+  | bc
