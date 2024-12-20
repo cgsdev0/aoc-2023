46dd9fbc51e0b3483ad9c41f606675f18b92e42d
Sarah Schulte <git@sarah.engineer>
2024-12-20 02:48:10 -0800

    cleanup

--- 2024/day05/p1.sh
+++ 2024/day05/p1.sh
@@ -5,60 +5,53 @@ FILE="$1"
 declare -A deps
 
 parse_rule() {
-  first="${1%%|*}"
-  second="${1##*|}"
-  if [[ -n "${deps[$second]}" ]]; then
-    first="${deps[$second]} $first"
+  IFS='|' read a b <<< "$line"
+  if [[ -n "${deps[$b]}" ]]; then
+    a="${deps[$b]} $a"
   fi
-  deps[$second]="$first"
+  deps[$b]="$a"
 }
 
 split() {
-   # Usage: split "string" "delimiter"
    IFS=$'\n' read -d "" -ra arr <<< "${1//$2/$'\n'}"
 }
 
 parse_update() {
-  local -a arr
-  local INVALID
-  local -a violations
-  split "$1" ,
+  local -a arr violations
+  local invalid
+  split "$line" ,
   local -a pages=("${arr[@]}")
   for page in "${pages[@]}"; do
     split "${deps[$page]}" ' '
     for v in "${violations[@]}"; do
       if [[ "$v" == "$page" ]]; then
-        INVALID=yes
+        invalid=yes
       fi
     done
     violations+=("${arr[@]}")
   done
-  [[ -z "$INVALID" ]] && echo "${pages[@]}"
+  [[ -z "$invalid" ]] && echo "${pages[@]}"
 }
 
 middle() {
-  local line
-  local -a arr
-  while IFS= read -r line; do
-    split "$line" ' '
-    LEN="${#arr[@]}"
-    ((LEN/=2))
-    echo ${arr["$LEN"]}
+  awk '{print $((NF+1)/2)}'
+}
+
+main() {
+  while read line; do
+      test -z "$line" && break
+      parse_rule
+  done
+
+  while read line; do
+      parse_update
   done
 }
 
-while IFS= read -r line; do
-  if [[ "$line" == "" ]]; then
-    section=2
-    continue
-  fi
+sum() {
+  paste -sd+ | bc
+}
 
-  if [[ $section -eq 2 ]]; then
-    parse_update "$line"
-  else
-    parse_rule "$line"
-  fi
-done < "$FILE" \
+main < "$FILE" \
   | middle \
-  | paste -sd+ \
-  | bc
+  | sum
